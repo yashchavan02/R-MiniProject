4df11a89fdd26fbbf0faa528c4f8ac33e29f4247
YASH CHAVAN <chavanyash127@gmail.com>
2024-10-21 22:07:51 +0530

    add comments

--- main/script.R
+++ main/script.R
@@ -1,12 +1,17 @@
 # Zomato data analysis using R
 
+# dataset path
 path = "public/ZomatoDataset.csv"
 
+# load dataset in data
 data = read.csv(path)
 
+# view dataset
 View(data) 
 
+# display top 6 entries
 head(data)
 
+# display bottom 6 entries
 tail(data)
 
