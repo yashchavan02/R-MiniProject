419223880144fae053d65c720174d813ec490025
YASH CHAVAN <chavanyash127@gmail.com>
2024-10-21 23:26:08 +0530

    add data exploration code

--- main/script.R
+++ main/script.R
@@ -1,17 +1,53 @@
 # Zomato data analysis using R
 
-# dataset path
-path = "public/ZomatoDataset.csv"
+# <- install and load packages ->
 
-# load dataset in data
+install.packages("dplyr")
+library(dplyr)
+
+# <- load & read data ->
+
+path = "public/ZomatoDataset.csv"
 data = read.csv(path)
 
-# view dataset
+# <- explore data ->
+
 View(data) 
-
-# display top 6 entries
 head(data)
-
-# display bottom 6 entries
 tail(data)
+glimpse(data)
+
+# <- categorical data exploration -> 
+
+unique(data $ Festival)
+unique(data $ Weather_conditions)
+unique(data $ Vehicle_condition)
+unique(data $ Type_of_order)
+unique(data $ Type_of_vehicle)
+unique(data $ multiple_deliveries)
+unique(data $ City)
+
+# <- data cleaning ->
+
+# drop unwanted columns 
+
+data = data %>% select(-Delivery_person_ID)
+data = data %>% select(-Restaurant_latitude)
+data = data %>% select(-Restaurant_longitude)
+data = data %>% select(-Delivery_location_latitude)
+data = data %>% select(-Delivery_location_longitude)
+
+glimpse(data)
+
+# drop rows where data is NaN
+
+data[data == "NaN"] = NA
+data = data %>% filter(!is.na(Festival) & !is.na(Weather_conditions) & !is.na(City))
+
+unique(data $ Festival)
+unique(data $ Weather_conditions)
+unique(data $ City)
+
+glimpse(data)
+
 
