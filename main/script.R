0ec14a28bd4b0931145a4ccd22c9cc493de41c5b
YASH CHAVAN <chavanyash127@gmail.com>
2024-10-22 00:15:37 +0530

    update script.R file 12:15 22 Oct 2024

--- main/script.R
+++ main/script.R
@@ -1,9 +1,11 @@
-# Zomato data analysis using R
+# Zomato data (2022) analysis using R 
 
 # <- install and load packages ->
 
 install.packages("dplyr")
+install.packages("tidyr")
 library(dplyr)
+library(tidyr)
 
 # <- load & read data ->
 
@@ -42,12 +44,36 @@ glimpse(data)
 # drop rows where data is NaN
 
 data[data == "NaN"] = NA
-data = data %>% filter(!is.na(Festival) & !is.na(Weather_conditions) & !is.na(City))
+data = data %>% filter(!is.na(Festival) & !is.na(Weather_conditions) & !is.na(City) & !is.na(multiple_deliveries))
 
 unique(data $ Festival)
 unique(data $ Weather_conditions)
 unique(data $ City)
+unique(data $ multiple_deliveries)
 
 glimpse(data)
 
+# separate the Order date column
 
+data = separate(data,col=Order_Date,int=c("Day","Month","Year"),sep="-")
+
+# drop unnecessary columns
+
+data = data %>% select(-Day)
+data = data %>% select(-Year)
+
+glimpse(data)
+
+# calculate time required to accept customer order
+
+data = data %>%
+  mutate(Time_Orderd = as.POSIXct(Time_Orderd, format = "%H:%M"),
+         Time_Order_picked = as.POSIXct(Time_Order_picked, format = "%H:%M"),
+         Time = as.numeric(difftime(Time_Order_picked, Time_Orderd, units = "mins")))
+
+# drop unnecessary columns
+
+data = data %>% select(-Time_Orderd)
+data = data %>% select(-Time_Order_picked)
+
+glimpse(data)
